0eb20c2cc1f93d4fc0358766a446484345023bf6
AngeZanetti <ange.zanetti@gmail.com>
2011-10-10 18:04:59 +0200

    Mod JSON avec sockets

--- app.js
+++ app.js
@@ -14,44 +14,63 @@ var  io = require('socket.io').listen(app);
 var task = {}; // liste des taches en cours
 	
 // Parse the JSON 
-function parseJSON () {
-	var data = fs.readFile('task.json', onReadFile);
+var parseJSON = function (jsonFile,callback) {
+	var data = fs.readFile(jsonFile, onReadFile);
 	function onReadFile(err, data) {
 		if (err) return onError(err);
-	console.log('data '+ data);		
-	this.list = JSON.parse(data);
+	list = JSON.parse(data);
+	callback(list);
 	}
 	
 }
-function editJSN() {
-	JSON.stringyfy();
+var ecritJSON = function(jsonFile, newtask, callback) {
+	fs.writeFile(jsonFile, newtask, function (err) {
+	  if (err) throw err;
+	  console.log('It\'s saved!');
+	});
+	
 }
-
 // Setup the server with Express 
 app.listen(300);
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+
+parseJSON('task.json',function(list) {
+	task = list;
+	console.log(task.todo.length);
+});
+
 // Open the websocket connection 
 io.sockets.on('connection', function (socket) {	
-	//task = parseJSON();
   	// Envoi le JSON à la creation de la page
 	//socket.emit('task', list);
+	
 	// Recoit les données position & texte du client
   	socket.on('change', function (data) {
-		// console.log('text ' + data.text);
-		// console.log('title ' + data.title);
-		// 	console.log('posx  ' + data.posx);
 		if (data.posx < 300) {
-			console.log('colonne1');
-			
+			if (task.todo.indexOf(data.title) != -1) {
+				task.todo.push({"title": data.title, "text":data.text});
+				console.log(task);
+				var jsonString = JSON.stringify(task); 
+				ecritJSON('task2.json', jsonString);
+			}
 		}else if (data.posx < 700) {
-			console.log('colonne 2');
+			if (task.doing.indexOf(data.title) != -1) {
+				task.doing.push({"title": data.title, "text":data.text});
+				console.log(task);
+				var jsonString = JSON.stringify(task);
+				ecritJSON('task2.json', jsonString);
+			}
 		}else {
-			console.log('colonne 3');
+			if (task.doing.indexOf(data.title) != -1) {
+				task.done.push({"title": data.title, "text":data.text});
+				console.log(task);
+				var jsonString = JSON.stringify(task);
+				ecritJSON('task2.json', jsonString);
+			}
 		}
 				
    	});
 });
-console.log('list ' + parseJSON.list);
