991c58996cece8effd4492adb74803321805fee3
AngeZanetti <ange.zanetti@gmail.com>
2011-10-10 14:55:35 +0200

    Getting sockets working

--- app.js
+++ app.js
@@ -2,46 +2,56 @@
 
 Server code for Qanban
 
-09/2011
+AngeZanetti - 2011
+
 */
 
+// import modules
 var fs = require('fs');
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var app = require('express').createServer();
+var  io = require('socket.io').listen(app);
 
-// Parse le JSON 
-// @@ Mettre le nom du fichier en arg de la fonction
+var task = {}; // liste des taches en cours
+	
+// Parse the JSON 
 function parseJSON () {
 	var data = fs.readFile('task.json', onReadFile);
-	
 	function onReadFile(err, data) {
 		if (err) return onError(err);
-	console.log("data "+ data);		
-	var list = JSON.parse(data);
-	return list;
+	console.log('data '+ data);		
+	this.list = JSON.parse(data);
 	}
 	
 }
+function editJSN() {
+	JSON.stringyfy();
+}
 
-// Port d'écoute du serv
+// Setup the server with Express 
 app.listen(300);
-
-
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-
-io.sockets.on('connection', function (socket) {
-	var task = parseJSON();
+// Open the websocket connection 
+io.sockets.on('connection', function (socket) {	
+	//task = parseJSON();
   	// Envoi le JSON à la creation de la page
-	socket.emit('task', task);
-	
+	//socket.emit('task', list);
 	// Recoit les données position & texte du client
   	socket.on('change', function (data) {
-		console.log("text " + data.text);		
+		// console.log('text ' + data.text);
+		// console.log('title ' + data.title);
+		// 	console.log('posx  ' + data.posx);
+		if (data.posx < 300) {
+			console.log('colonne1');
+			
+		}else if (data.posx < 700) {
+			console.log('colonne 2');
+		}else {
+			console.log('colonne 3');
+		}
+				
    	});
-	// socket.emit('change', { pos: ui.position, text: ui.item.text() });
-
 });
-
+console.log('list ' + parseJSON.list);
